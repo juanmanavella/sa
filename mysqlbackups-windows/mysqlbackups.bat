04cfd28a525c678d0f84652f289d877525b0fba2
juanmanavella <juanmanavella@gmail.com>
2017-03-15 09:46:02 -0300

    Adding dates to filenames

--- mysqlbackups-windows/mysqlbackups.bat
+++ mysqlbackups-windows/mysqlbackups.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 c:\xampp\mysql\bin\mysqldump -u %1 -p%2 %3 > c:\mysqlbackups\%3.sql
-rar a -ep1 -m5 -df c:\mysqlbackups\%3.rar c:\mysqlbackups\%3.sql
+rar a -ep1 -m5 -df c:\mysqlbackups\%3_%date:~-7,2%-%date:~-10,2%-%date:~-4,4%.rar c:\mysqlbackups\%3.sql
